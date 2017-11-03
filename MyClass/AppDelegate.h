91aeacf6cb5fcace5fee25512592d57de0cc75a7
陈锐 <ChenRui@chenrui.local>
2017-11-03 11:33:02 +0800

    Initial Commit

---
+++ MyClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyClass
+//
+//  Created by 陈锐 on 2017/11/3.
+//  Copyright © 2017年 陈锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyClass
+//
+//  Created by 陈锐 on 2017/11/3.
+//  Copyright © 2017年 陈锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
